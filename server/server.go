634f096d212c665920cb02c333717f3682ee2323
246859 <2633565580@qq.com>
2023-01-11 18:17:44 +0800

    feat(server) : 应用启动大致流程完善

--- server/server.go
+++ server/server.go
@@ -1,17 +1,27 @@
 package server
 
 import (
+	"context"
 	"errors"
 	"flag"
+	"liteserver/utils"
+	"log"
+	"net/http"
 
 	"github.com/fsnotify/fsnotify"
+	"go.uber.org/zap"
 	"liteserver/config"
 	"liteserver/global"
 	"liteserver/initialize"
 )
 
+var (
+	defaultConfigPath = "./config.yml"
+)
+
 type Server struct {
 	ConfigPath string
+	server     *http.Server
 }
 
 // loadConfig
@@ -45,12 +55,26 @@ func (s *Server) loadEnv() error {
 	if global.Config == nil {
 		return errors.New("应用环境加载异常")
 	}
+	// 初始化日志
 	global.Logger = initialize.InitZap(global.Config.ZapConfig)
-	global.Logger.Info("日志初始化完成")
-	global.Logger.Error("test")
+	// 初始化Redis连接
 	global.Redis = initialize.InitRedis(global.Config.RedisConfig)
+	// 初始化GORM和数据库
 	global.GormDBGroup = initialize.InitGorm(global.Config.DataBaseConfig)
+	// 初始化Http服务器
+	s.server = initialize.InitHttpServer(global.Config.ServerConfig)
 	return nil
+}
+
+// runHttpServer
+// @Date 2023-01-11 17:47:36
+// @Description: 运行Http服务器
+func (s *Server) runHttpServer() {
+	defer s.ShutDown()
+	err := s.server.ListenAndServe()
+	if err != nil {
+		zap.L().Error("应用启动失败，即将关闭", zap.Error(err))
+	}
 }
 
 // Run
@@ -58,27 +82,40 @@ func (s *Server) loadEnv() error {
 // @Description: 应用启动
 // @Receiver: s *Server
 func (s *Server) Run() {
+	utils.LogBanner()
 	// 加载配置文件
 	err := s.loadConfig()
 	if err != nil {
-		panic(err)
+		log.Panicln("配置文件加载失败:", err)
 	}
 
 	// 加载应用环境
 	err = s.loadEnv()
 	if err != nil {
-		panic(err)
+		log.Panicln("应用环境初始化失败:", err)
 	}
-	s.Refresh()
+	s.refresh()
+	s.runHttpServer()
 }
 
-func (s *Server) Refresh() {
+// ShutDown
+// @Date: 2023-01-09 23:28:26
+// @Description: 优雅的关闭应用
+// @Receiver: s *Server
+func (s *Server) ShutDown() {
+	err := s.server.Shutdown(context.Background())
+	if err != nil {
+		zap.L().Fatal("Http服务器无法正常关闭，应用程序将立即停止", zap.Error(err))
+	}
+}
+
+func (s *Server) refresh() {
 	global.Viper.WatchConfig()
 	global.Viper.OnConfigChange(func(in fsnotify.Event) {
 		// 加载应用环境
 		err := s.loadEnv()
 		if err != nil {
-			panic(err)
+			log.Panicln("应用环境刷新失败:", err)
 		}
 	})
 }
@@ -88,7 +125,7 @@ func (s *Server) Refresh() {
 // @Description: 使用命令行标志输入配置文件路径
 // @Receiver: s *Server
 func (s *Server) RunWithFlag() {
-	configPath := flag.String("cfg", "./config.yml", "服务器配置文件")
+	configPath := flag.String("cfg", defaultConfigPath, "服务器配置文件")
 	flag.Parse()
 	s.ConfigPath = *configPath
 	s.Run()
@@ -99,6 +136,6 @@ func (s *Server) RunWithFlag() {
 // @Description: 使用默认的配置文件路径
 // @Receiver: s *Server
 func (s *Server) RunWithDefault() {
-	s.ConfigPath = "./config.yml"
+	s.ConfigPath = defaultConfigPath
 	s.Run()
 }
