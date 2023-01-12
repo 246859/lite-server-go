0796bd8946cdc9f05b4f02cab0cd7ade7968712e
246859 <2633565580@qq.com>
2023-01-12 20:37:06 +0800

    feat(utils) : 新增Must工具

--- utils/must.go
+++ utils/must.go
@@ -1,11 +1,34 @@
 package utils
 
-// Must
+import (
+	"go.uber.org/zap"
+	"liteserver/global"
+	"log"
+)
+
+// MustOrPanic
 // @Date 2023-01-12 18:23:11
 // @Param f func() error
 // @Description: 必须完成某个操作，否则就panic
-func Must(f func() error) {
+func MustOrPanic(f func() error) {
 	if err := f(); err != nil {
 		panic(err)
+	}
+}
+
+// MustOrLogPanic
+// @Date 2023-01-12 19:15:33
+// @Param f func() error
+// @Param msg string
+// @Param field ...zap.Field
+// @Description: 必须完成某一个操作，否则就panic并记录日志
+func MustOrLogPanic(f func() error, msg string, field ...zap.Field) {
+	if err := f(); err != nil {
+		if global.Logger != nil {
+			field = append(field, zap.Error(err))
+			zap.L().Panic(msg, field...)
+		} else {
+			log.Panicln(msg, err)
+		}
 	}
 }
